c327ce8046d47811f01c1cf29e908c4e0220783b
huzhigan <1364897921@qq.com>
2026-08-10 17:54:46 +0800

    fix: 开启viteEnvironmentApi规避ssr:false下dev崩溃

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   ssr: false, // 内部工具无SEO需求,关闭SSR改SPA,服务器不再每次请求渲染HTML,大幅提升访问速度
   devtools: { enabled: false },
+  experimental: {
+    viteEnvironmentApi: true // 规避 ssr:false 下 dev 崩溃: No entry found in rollupOptions.input
+  },
   devServer: { port: 3005 },
   css: ['~/assets/css/main.css'],
   app: {
