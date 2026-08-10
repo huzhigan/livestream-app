a72a651659efd1abcbc0b69a909a0585f6e21248
huzhigan <1364897921@qq.com>
2026-08-10 17:31:07 +0800

    refactor: 关闭SSR切换SPA模式,提升页面访问速度

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  ssr: false, // 内部工具无SEO需求,关闭SSR改SPA,服务器不再每次请求渲染HTML,大幅提升访问速度
   devtools: { enabled: false },
   devServer: { port: 3005 },
   css: ['~/assets/css/main.css'],
